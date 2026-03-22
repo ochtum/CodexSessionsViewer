9c5ab99323b834559b5f0a4fa5a261ee16f8fd29
ochtum <okutoumu@gmail.com>
2026-03-22 10:54:57 +0900

    ラベル情報の保存先変更

--- publish.ps1
+++ publish.ps1
@@ -25,7 +25,7 @@ if (-not (Test-Path $projectPath)) {
 }
 
 if ($CleanOutput -and (Test-Path $outputDir)) {
-    Get-ChildItem -Path $outputDir -Force | Remove-Item -Recurse -Force
+    Get-ChildItem -Path $outputDir -Force | Where-Object { $_.Name -ne ".cache" } | Remove-Item -Recurse -Force
 }
 
 New-Item -ItemType Directory -Path $outputDir -Force | Out-Null
@@ -87,6 +87,7 @@ if (-not (Test-Path $launcherPath)) {
     'setlocal'
     ('title {0}' -f $consoleWindowTitle)
     'pushd "%~dp0" >nul'
+    'set "SESSIONS_VIEWER_APP_ROOT=%CD%"'
     ('.\payload\{0} %*' -f $launcherName)
     'set "EXIT_CODE=%ERRORLEVEL%"'
     'popd >nul'
